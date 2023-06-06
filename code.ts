6cd59c46a3bb836137e2ef68606af85e2a735fa1
Masatoshi Uchida <uchidamasa.design@gmail.com>
2023-06-07 01:43:19 +0900

    とりあえずコンポーネントからプロパティのIDが取れることがわかった

--- code.ts
+++ code.ts
@@ -1,11 +1,62 @@
 const opsions = { width: 320, height: 320 };
+//const nodes: SceneNode[] = [];
+//nodes.push(jsonText);
+//figma.viewport.scrollAndZoomIntoView(nodes);
+
+/*計画
+  コンポーネントを選択
+  json入れる
+  ON
+  jsonParce
+  コンポーネントからcomponentPropertyDefinitions
+  Object.keyでkeyのリストを取り出す(for...in構文でもオブジェクトのkeyだけ取れる)
+  そのリストを使ってインスタンスのsetPropertiesを使って値をブっ込む
+*/
+
 figma.showUI(__html__, opsions);
+
+//テキストオブジェクトを画面に出す
+const textObjectCreate = (json: string) => {
+  const jsonText = figma.createText();
+  jsonText.fontName = { family: "Noto Sans JP", style: "Regular" };
+  jsonText.fontSize = 14;
+  jsonText.characters = json;
+  figma.currentPage.appendChild(jsonText);
+};
+
+//コンポーネントのプロパティのIDを手に入れる
+const getComponentPropertysId = (node: any) => {
+  const hoge = node.componentPropertyDefinitions;
+  console.log(hoge);
+
+  const keys = Object.keys(hoge);
+  console.log(`このコンポーネントのキー：${keys}`);
+};
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === "throw-json") {
+    const nodes: readonly SceneNode[] = figma.currentPage.selection;
+    console.log(`nodesの中身:${nodes}`);
+
+    if (nodes.length) {
+      nodes.forEach((node) => {
+        /*if (node.type === "INSTANCE") {
+          console.log(`nodeの${index}番：` + node);
+          const hoge = node.componentProperties;
+          console.log(hoge);
+
+          node.setProperties({ "title#1:0": "タイトルテストだよ" });
+        }*/
+
+        if (node.type === "COMPONENT") {
+        }
+      });
+    }
+
     try {
       const json = JSON.parse(msg.pureInput);
       console.log(json);
+      console.log(figma.currentPage.selection);
       figma.ui.postMessage("success");
     } catch (e) {
       console.log(e);
@@ -17,27 +68,10 @@ figma.ui.onmessage = (msg) => {
       await figma.loadFontAsync({ family: "Noto Sans JP", style: "Regular" });
     };
 
-    //テキストオブジェクトを画面に出す
-    const textObjectCreate = () => {
-      const nodes: SceneNode[] = [];
-
-      //
-      const jsonText = figma.createText();
-      jsonText.fontName = { family: "Noto Sans JP", style: "Regular" };
-      jsonText.fontSize = 14;
-      jsonText.characters = msg.pureInput;
-      nodes.push(jsonText);
-
-      figma.currentPage.appendChild(jsonText);
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    };
-
     //テキストオブジェクト作成開始
     fontLoader().then(() => {
-      textObjectCreate();
+      textObjectCreate(msg.pureInput);
     });
-
-    //figma.currentPage.selection = nodes;
   }
 
   //figma.closePlugin();
