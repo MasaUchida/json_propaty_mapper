7d443abc6f278ece9784641419052e782159cb38
Masatoshi Uchida <uchidamasa.design@gmail.com>
2023-06-12 02:08:00 +0900

    コード完成

--- code.ts
+++ code.ts
@@ -1,79 +1,97 @@
 const opsions = { width: 320, height: 320 };
-//const nodes: SceneNode[] = [];
-//nodes.push(jsonText);
-//figma.viewport.scrollAndZoomIntoView(nodes);
-
-/*計画
-  コンポーネントを選択
-  json入れる
-  ON
-  jsonParce
-  コンポーネントからcomponentPropertyDefinitions
-  Object.keyでkeyのリストを取り出す(for...in構文でもオブジェクトのkeyだけ取れる)
-  そのリストを使ってインスタンスのsetPropertiesを使って値をブっ込む
-*/
-
 figma.showUI(__html__, opsions);
+
+//フォント読み込み
+const fontLoader = async () => {
+  await figma.loadFontAsync({ family: "Noto Sans JP", style: "Regular" });
+};
 
 //テキストオブジェクトを画面に出す
 const textObjectCreate = (json: string) => {
   const jsonText = figma.createText();
   jsonText.fontName = { family: "Noto Sans JP", style: "Regular" };
-  jsonText.fontSize = 14;
+  jsonText.fontSize = 16;
   jsonText.characters = json;
   figma.currentPage.appendChild(jsonText);
 };
 
-//コンポーネントのプロパティのIDを手に入れる
-const getComponentPropertysId = (node: ComponentNode) => {
-  const hoge = node.componentPropertyDefinitions;
-  console.log(hoge);
+//入力データのキーをインスタンスのpropertyIdデータにすり替える
 
-  const keys = Object.keys(hoge);
-  console.log(`このコンポーネントのキー：${keys}`);
+//なんでも入れられるobjectの型
+type objType = {
+  [props: string]: any;
+};
+
+const swapInputObjectKeyToInstanceId = (
+  //改良できそう　毎回配列で返す必要はなさそう　indexをとって、keys[index]にして単発にすれば楽そうだぞ
+  data: any[],
+  keys: string[]
+): objType[] => {
+  const output = [];
+
+  for (const obj of data) {
+    const transformObject: objType = {};
+
+    for (const key of keys) {
+      //keyからid部分(#0:0)をカット
+      const property = key.replace(/#.*/, "");
+      //objectプロパティの追加
+      transformObject[key] = obj[property];
+    }
+
+    output.push(transformObject);
+  }
+
+  return output;
 };
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === "throw-json") {
     const nodes: readonly SceneNode[] = figma.currentPage.selection;
-    console.log(`nodesの中身:${nodes}`);
+    const json = JSON.parse(msg.pureInput);
 
     if (nodes.length) {
+      try {
+        const json = JSON.parse(msg.pureInput);
+        figma.ui.postMessage("success");
+      } catch (e) {
+        console.log(e);
+        figma.ui.postMessage("error");
+      }
+
       nodes.forEach((node) => {
-        /*if (node.type === "INSTANCE") {
-          console.log(`nodeの${index}番：` + node);
-          const hoge = node.componentProperties;
-          console.log(hoge);
-
-          node.setProperties({ "title#1:0": "タイトルテストだよ" });
-        }*/
-
         if (node.type === "COMPONENT") {
-          getComponentPropertysId(node);
+          const componentArray: any[] = json.components;
+          const componentsArrayLength = json.components.length;
+
+          for (let i = 0; i < componentsArrayLength; i++) {
+            //インスタンスを作成
+            const instance = node.createInstance();
+
+            //レイヤー名をつける
+            instance.name = componentArray[i].name;
+
+            //インスタンスのプロパティのidの配列を作成
+            const instancePropertyIds = Object.keys(
+              instance.componentProperties
+            );
+
+            //id配列と入力objectのvalueでsetするobjectを作成
+            const setProp = swapInputObjectKeyToInstanceId(
+              componentArray,
+              instancePropertyIds
+            );
+
+            //インスタンスにセット
+            instance.setProperties(setProp[i]);
+          }
         }
       });
     }
-
-    try {
-      const json = JSON.parse(msg.pureInput);
-      console.log(json);
-      console.log(figma.currentPage.selection);
-      figma.ui.postMessage("success");
-    } catch (e) {
-      console.log(e);
-      figma.ui.postMessage("error");
-    }
-
-    //フォント読み込み
-    const fontLoader = async () => {
-      await figma.loadFontAsync({ family: "Noto Sans JP", style: "Regular" });
-    };
 
     //テキストオブジェクト作成開始
     fontLoader().then(() => {
       textObjectCreate(msg.pureInput);
     });
   }
-
-  //figma.closePlugin();
 };
