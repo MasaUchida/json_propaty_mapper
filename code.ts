529666107e29114c6c3048d64babc7cd907c9eb0
Masatoshi Uchida <uchidamasa.design@gmail.com>
2023-06-20 00:33:53 +0900

    関数分割

--- code.ts
+++ code.ts
@@ -1,6 +1,14 @@
 const opsions = { width: 320, height: 320 };
 figma.showUI(__html__, opsions);
 
+//インスタンスの型
+type instanceType = {
+  name: string;
+  [key: string]: string | boolean;
+};
+
+type instanceValue = Omit<instanceType, "name">;
+
 //フォント読み込み
 const fontLoader = async () => {
   await figma.loadFontAsync({ family: "Noto Sans JP", style: "Regular" });
@@ -15,19 +23,40 @@ const textObjectCreate = (json: string) => {
   figma.currentPage.appendChild(jsonText);
 };
 
-//インスタンスの型
-type instanceType = {
-  name: string;
-  [key: string]: string | boolean;
+/**
+ * インスタンスを放り込むと、放り込んだインスタンスのプロパティのid付き文字列の配列を返却
+ * @param {InstanceNode} targetInstance - 対象のインスタンス
+ * @returns {string[]} instancePropertyIds - プロパティのid付きstring配列を返却 ["hoge#0:0" , "fuga#0:0"]
+ */
+const getInstancePropertyIds = (targetInstance: InstanceNode): string[] => {
+  const instancePropertyIds = Object.keys(targetInstance.componentProperties);
+  return instancePropertyIds;
 };
 
-type instanceValue = Omit<instanceType, "name">;
+/**
+ * id付きkeyとpropertyのvalueでできたオブジェクトを返却する
+ * @param keyWithId
+ * @param targetInstance
+ * @returns object - {"hoge#0:0":"hogehoge"}
+ */
+const makeSettingObject = (keyWithId: string, targetInstance: instanceType) => {
+  const outputObject: instanceValue = {};
+
+  //keyからid部分(#0:0)をカット
+  const valueKey = keyWithId.substring(0, keyWithId.lastIndexOf("#"));
+
+  //{"hoge#0:0" : "hogehoge"}を作成
+  outputObject[keyWithId] = targetInstance[valueKey];
+
+  return outputObject;
+};
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === "throw-json") {
     const nodes: readonly SceneNode[] = figma.currentPage.selection;
     let json: any;
 
+    //jsonの記述に問題がないかの判定
     if (nodes.length) {
       try {
         json = JSON.parse(msg.pureInput);
@@ -37,43 +66,28 @@ figma.ui.onmessage = (msg) => {
         figma.ui.postMessage("error");
       }
 
+      ////main////
       nodes.forEach((node) => {
         if (node.type === "COMPONENT") {
           const instancesArray: instanceType[] = json.instances;
-          const swapedKeyValueObject: instanceValue = {};
-
-          console.log(instancesArray);
 
           instancesArray.forEach((instance) => {
             //インスタンスを作成
             const newInstance = node.createInstance();
 
             //インスタンスのプロパティのidの配列を作成
-            //instancePropertyIds => ["hoge#0:0" , "fuga#0:0"]
-            const instancePropertyIds = Object.keys(
-              newInstance.componentProperties
-            );
-
-            console.log("instancePropertyIds", instancePropertyIds);
+            const ids = getInstancePropertyIds(newInstance);
 
             //レイヤー名をつける
             newInstance.name = instance.name;
 
-            //objectプロパティ {"hoge#0:0":"hogehoge"}を捻り出す
-            instancePropertyIds.forEach((keyWithId) => {
-              //keyからid部分(#0:0)をカット
-              const valueKey = keyWithId.substring(
-                0,
-                keyWithId.lastIndexOf("#")
-              );
+            ids.forEach((id) => {
+              //setに必要な形にオブジェクトを整形
+              const settingObject = makeSettingObject(id, instance);
 
-              //instancePropertyIdsをkeyにする
-              //このkeyからID部分を無くしたものをkeyにしてinstanceからvalueを呼び出す
-              swapedKeyValueObject[keyWithId] = instance[valueKey];
+              //インスタンスにセット
+              newInstance.setProperties(settingObject);
             });
-
-            //インスタンスにセット {"hoge#0:0":"内容"}
-            newInstance.setProperties(swapedKeyValueObject);
           });
         }
       });
